98252fc7930f1cdacd710a41d4ac67a436e059de
Elidor Varosi <elidorv@outlook.com>
2021-07-23 09:32:43 +0200

    Update regressor.py
    Fixed the IO problem related to fast api reloading the XGBoost model every time by invoking the whole API script instead of only the request endpoint function(different from Flask)

--- PythonBenchmark/regressor.py
+++ PythonBenchmark/regressor.py
@@ -4,19 +4,22 @@ import numpy as np
 
 class Predictor:
     def __init__(self):
-        self.loaded_model = XGBRegressor()
-        self.booster = Booster()
-        self.booster.load_model('xg.model')
-        self.loaded_model._Booster = self.booster
 
-    def predict(self,val1,val2,val3,val4,val5,val6,val7,val8):
-        ls = [val1,val2,val3,val4,val5,val6,val7,val8]
+        if 'self.loaded_model' in globals():
+            print('')
+        else:
+            self.loaded_model = XGBRegressor()
+            self.booster = Booster()
+            self.booster.load_model('xg.model')
+            self.loaded_model._Booster = self.booster
+
+
+    def predict(self,ls):
+        #ls = [val1,val2,val3,val4,val5,val6,val7,val8]
         #ls = ls.append(val1,val2,val3,val4,val5,val6,val7,val8)
         dt = np.array(ls)
-        dt=dt.reshape(1,-1)
+        #If the list conains only one item add the axis dimension so the model doesn't crash
+        if len(ls)==1:
+            dt=dt.reshape(1,-1)
         yp=self.loaded_model.predict(dt)
-        return yp
-
-
-pred = Predictor()
-print(pred.predict(2	,0	,110	,1 ,1640	,0.01	,223	,1978))
+        return yp.tolist()
